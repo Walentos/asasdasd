b54e3ccb36a41dae565706c6169006dbf14df2ff
Walentos <46710351+Walentos@users.noreply.github.com>
2019-01-15 13:51:08 +0300

    Add files via upload

---
+++ Projem/login_registerjava.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function yazdir() {
+    var myElem = document.getElementById("kadi");
+    var myInner.load('Projemain.html')='';
+   
+        myInner = myInner + '<li  class="list-group-item" ' + myElem + '">';
+        
+    
